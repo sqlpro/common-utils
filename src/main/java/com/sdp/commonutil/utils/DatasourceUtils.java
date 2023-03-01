89f35844a23f41e64bb839d001a0a2c6be43e29f
Jaeeun, Lee <hijaeeun@amazon.com>
2023-03-01 23:06:32 +0900

    Add DatasourceUtils

---
+++ src/main/java/com/sdp/commonutil/utils/DatasourceUtils.java
@@ -0,0 +1,10 @@
+package com.sdp.commonutil.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatasourceUtils {
+    public static List<Object> getConnections() {
+        return new ArrayList<>();
+    }
+}
